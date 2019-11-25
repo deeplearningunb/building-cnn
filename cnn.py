52ed390621448012b74713b01e11a0278fc45ae2
João Pedro Sconetto <sconetto.joao@gmail.com>
2019-11-25 00:31:10 -0300

    feat(cnn): update cnn
    - last update to the code
    - style fixing
    
    Signed-off-by: João Pedro Sconetto <sconetto.joao@gmail.com>

--- cnn.py
+++ cnn.py
@@ -9,6 +9,8 @@
 # Installing Keras
 # pip install --upgrade keras
 
+# Convolutional Neural Network
+
 # Part 1 - Building the CNN
 
 # Importing the Keras libraries and packages
@@ -22,48 +24,72 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation="selu"))
 
 # Step 2 - Pooling
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Adding a second convolutional layer
-classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Conv2D(32, (3, 3), activation="relu"))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Adding a third convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation="sigmoid"))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Step 3 - Flattening
 classifier.add(Flatten())
 
 # Step 4 - Full connection
-classifier.add(Dense(units = 128, activation = 'relu'))
-classifier.add(Dense(units = 1, activation = 'sigmoid'))
+classifier.add(Dense(units=128, activation="relu"))
+classifier.add(Dense(units=1, activation="sigmoid"))
 
 # Compiling the CNN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(
+    optimizer="rmsprop", loss="binary_crossentropy", metrics=["accuracy", "mse"]
+)
 
 # Part 2 - Fitting the CNN to the images
 
 from keras.preprocessing.image import ImageDataGenerator
 
-train_datagen = ImageDataGenerator(rescale = 1./255,
-                                   shear_range = 0.2,
-                                   zoom_range = 0.2,
-                                   horizontal_flip = True)
+train_datagen = ImageDataGenerator(
+    rescale=1.0 / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True
+)
 
-test_datagen = ImageDataGenerator(rescale = 1./255)
+test_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
-training_set = train_datagen.flow_from_directory('dataset/training_set',
-                                                 target_size = (64, 64),
-                                                 batch_size = 32,
-                                                 class_mode = 'binary')
+training_set = train_datagen.flow_from_directory(
+    "dataset/training_set", target_size=(64, 64), batch_size=32, class_mode="binary"
+)
 
-test_set = test_datagen.flow_from_directory('dataset/test_set',
-                                            target_size = (64, 64),
-                                            batch_size = 32,
-                                            class_mode = 'binary')
+test_set = test_datagen.flow_from_directory(
+    "dataset/test_set", target_size=(64, 64), batch_size=32, class_mode="binary"
+)
 
-classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000,
-                         epochs = 25,
-                         validation_data = test_set,
-                         validation_steps = 2000)
+classifier.fit_generator(
+    training_set,
+    steps_per_epoch=8000,
+    epochs=25,
+    validation_data=test_set,
+    validation_steps=2000,
+)
+
+# Part 3 - Making new predictions
+
+import numpy as np
+from keras.preprocessing import image
+
+test_image = image.load_img(
+    "dataset/single_prediction/cat_or_dog_2.jpeg", target_size=(64, 64)
+)
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis=0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = "dog"
+else:
+    prediction = "cat"
+
+print(prediction)
