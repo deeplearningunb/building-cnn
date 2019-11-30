19a0b599cca4032d482de92346d640047a459f40
Lucas <lucasfcm9@gmail.com>
2019-11-29 23:18:36 -0300

    add results and change some parameters in functions

--- cnn.py
+++ cnn.py
@@ -39,7 +39,7 @@ classifier.add(Dense(units = 128, activation = 'relu'))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 # Compiling the CNN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer = 'sgd', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Part 2 - Fitting the CNN to the images
 
@@ -54,16 +54,17 @@ test_datagen = ImageDataGenerator(rescale = 1./255)
 
 training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                  target_size = (64, 64),
-                                                 batch_size = 32,
+                                                 batch_size = 50,
                                                  class_mode = 'binary')
 
 test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             target_size = (64, 64),
-                                            batch_size = 32,
+                                            batch_size = 45,
                                             class_mode = 'binary')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000,
-                         epochs = 25,
+                         steps_per_epoch = 2000,
+                         workers = 8,
+                         epochs = 15,
                          validation_data = test_set,
                          validation_steps = 2000)
