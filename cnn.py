465430760e1271f1fb7ba269e0a399c3838348a7
Guilherme Augusto <guilherme.francais@gmail.com>
2019-12-10 16:23:02 -0300

    Improve accuracy in more than 80%

--- cnn.py
+++ cnn.py
@@ -31,11 +31,16 @@ classifier.add(MaxPooling2D(pool_size = (2, 2)))
 classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
+# Adding a third convolutional layer
+
+classifier.add(Conv2D(128, (3, 3), activation = 'relu'))
+
 # Step 3 - Flattening
 classifier.add(Flatten())
 
 # Step 4 - Full connection
 classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 64, activation = 'relu'))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 # Compiling the CNN
@@ -54,16 +59,16 @@ test_datagen = ImageDataGenerator(rescale = 1./255)
 
 training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                  target_size = (64, 64),
-                                                 batch_size = 32,
+                                                 batch_size = 8,
                                                  class_mode = 'binary')
 
 test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             target_size = (64, 64),
-                                            batch_size = 32,
+                                            batch_size = 8,
                                             class_mode = 'binary')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000,
+                         steps_per_epoch = 1000,
                          epochs = 25,
                          validation_data = test_set,
                          validation_steps = 2000)
