39e8a5da35547e836e64161886f39c12693d4a6e
Diego Dorgam <diego.dorgam@rocket.chat>
2020-09-19 17:06:05 -0300

    atualização de versão

--- cnn_homework_solution.py
+++ cnn_homework_solution.py
@@ -22,8 +22,7 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(
-        Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
 
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
@@ -69,11 +68,16 @@ classifier.fit_generator(training_set,
                          validation_data = test_set,
                          validation_steps = 2000)
 
+# Part 3 - Making new predictions
 
-
-
-
-
-
-
-
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
