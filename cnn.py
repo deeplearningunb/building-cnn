970801c0b69de02f6877f4e247dc3a92988a888c
= <=>
2021-08-26 17:42:30 -0300

    Construindo uma CNN

--- cnn.py
+++ cnn.py
@@ -23,7 +23,7 @@ classifier = Sequential()
 
 # Step 1 - Convolution
 classifier.add(
-        Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+        Conv2D(32, (5, 5), input_shape = (64, 64, 3), activation = 'relu'))
 
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
@@ -37,10 +37,17 @@ classifier.add(Flatten())
 
 # Step 4 - Full connection
 classifier.add(Dense(units = 128, activation = 'relu'))
+
+# Adding the second hidden layer
+classifier.add(Dense(units = 128, activation = 'relu'))
+
+# Step 5 - Output Layer
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 # Compiling the CNN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+
 
 # Part 2 - Fitting the CNN to the images
 
@@ -63,11 +70,25 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             batch_size = 32,
                                             class_mode = 'binary')
 
-classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000,
-                         epochs = 25,
-                         validation_data = test_set,
-                         validation_steps = 2000)
+# Training the CNN on the Training set and evaluating it on the Test set
+classifier.fit(x = training_set, validation_data = test_set, epochs = 25)
+
+# Part 4 - Making a single prediction
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+  prediction = 'dog'
+else:
+  prediction = 'cat'
+
+print(prediction)
+
+
 
 
 
