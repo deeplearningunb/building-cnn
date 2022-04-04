57ed357aea08148159f5a0836782d025d0d4c6e7
Mickaele Musialowski <mickaelemusialowski38@gmail.com>
2022-04-04 09:28:07 -0300

    alterações

--- cnn_homework_solution.py
+++ cnn_homework_solution.py
@@ -13,8 +13,8 @@
 
 # Importing the Keras libraries and packages
 from keras.models import Sequential
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
+from keras.layers import Conv3D
+from keras.layers import MaxPooling3D
 from keras.layers import Flatten
 from keras.layers import Dense
 
@@ -22,20 +22,20 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Conv2D(32, (3, 3), input_shape = (70,70, 3), activation = 'relu'))
 
 # Step 2 - Pooling
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(MaxPooling3D(pool_size = (3)))
 
 # Adding a second convolutional layer
 classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(MaxPooling2D(pool_size = (2,2)))
 
 # Step 3 - Flattening
 classifier.add(Flatten())
 
 # Step 4 - Full connection
-classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 512, activation = 'relu'))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 # Compiling the CNN
@@ -53,12 +53,12 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
 training_set = train_datagen.flow_from_directory('dataset/training_set',
-                                                 target_size = (64, 64),
+                                                 target_size = (70, 70)
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
 test_set = test_datagen.flow_from_directory('dataset/test_set',
-                                            target_size = (64, 64),
+                                            target_size = (70, 70),
                                             batch_size = 32,
                                             class_mode = 'binary')
 
@@ -72,7 +72,7 @@ classifier.fit_generator(training_set,
 
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (70, 70))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = classifier.predict(test_image)
